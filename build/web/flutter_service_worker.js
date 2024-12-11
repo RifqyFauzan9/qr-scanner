75c90d1d0e74d485378253dbdd2d395b9f76eea2
Rifqy Fauzan <r1fqyf4uz4n@gmail.com>
2024-12-11 08:16:53 +0700

    mampulko

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "32c6a57c7ef0cdef583f9397f767723b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "50276159f75dbdde6be27b868c3cba4d",
+"flutter_bootstrap.js": "9d2c74b3e24e64430b104950bd655a9e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
